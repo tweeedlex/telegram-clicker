2312efdd1688a22368443d1aa34288f3235ca27a
Artemka1806 <tyschenkoartem2580@gmail.com>
2024-06-07 18:54:34 +0300

    save clicks

--- frontend/src/pages/Main/Main.jsx
+++ frontend/src/pages/Main/Main.jsx
@@ -1,10 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useSelector} from "react-redux";
 import styles from "./Main.module.scss";
 
 const Main = () => {
   const telegramData = useSelector((state) => state.telegramData);
   const [taps, setTaps] = useState(0);
+  const [energy, setEnergy] = useState(1500);
+  const saveTaps = () => {
+    setTaps(taps => taps + 1)
+    setEnergy(energy => energy - 1)
+    localStorage.setItem("taps", taps + 1);
+    localStorage.setItem("energy", energy - 1);
+  }
+  useEffect(() => {
+    if (localStorage.getItem("taps") && localStorage.getItem("taps") != null){
+      setTaps(+localStorage.getItem("taps"))
+      setEnergy(+localStorage.getItem("energy"))
+    }
+  }, [])
 
   return (
     <div className={"page " + styles.mainPage}>
@@ -14,7 +27,7 @@ const Main = () => {
         </p>
       </header>
       <div className={styles.block}>
-        <button className={styles.button} onClick={() => setTaps(taps => taps + 1)}>
+        <button className={styles.button} onClick={saveTaps}>
           <img src="https://upload.wikimedia.org/wikipedia/commons/3/37/African_Bush_Elephant.jpg" alt="slon" width={250} />
         </button>
         <div>
@@ -26,6 +39,11 @@ const Main = () => {
           </p>
           <p>
             times.
+          </p>
+        </div>
+        <div>
+          <p>
+            Energy: {energy}
           </p>
         </div>
       </div>
