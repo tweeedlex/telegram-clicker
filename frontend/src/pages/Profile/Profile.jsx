7394163fd8ecfb8658ed236333ba5cca25f1940f
Artemka1806 <tyschenkoartem2580@gmail.com>
2024-06-14 18:53:45 +0300

    referral fix

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -3,18 +3,26 @@ import { useSelector, useDispatch } from "react-redux";
 
 const Profile = () => {
 	const telegramData = useSelector((state) => state.telegramData)
+	const urlInput = document.getElementById("refURL");
 
 	const copyRefURL = () => {
-		let url = document.getElementById("refURL");
-		url.select();
-  		document.execCommand("copy");
+		if (urlInput){
+			urlInput.select();
+	  		document.execCommand("copy");
+  		}
 	}
+
+	useEffect(() => {
+		if (urlInput){
+    		urlInput.value = telegramData.refURL;
+		}
+  	}, [telegramData])
 
 	return (
 		<>
 			Invite friends:
 			<div>
-				<input id="refURL" type="text" readonly="true" value={telegramData.refURL} />
+				<input id="refURL" type="text" readOnly="true" value={telegramData.refURL} />
 				<button onClick={copyRefURL}>Copy</button>
 			</div>
 		</>
